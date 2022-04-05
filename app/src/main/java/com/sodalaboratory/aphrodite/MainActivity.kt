be25df8c8bd4a4917f872ba4d94aba5b85a9c930
dislikesoda <tansuanyinliao8888@gmail.com>
2022-04-05 17:34:43 +0800

    [update] add ViewPager2

--- app/src/main/java/com/sodalaboratory/aphrodite/MainActivity.kt
+++ app/src/main/java/com/sodalaboratory/aphrodite/MainActivity.kt
@@ -2,21 +2,39 @@ package com.sodalaboratory.aphrodite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.sodalaboratory.aphrodite.ui.PersonListFragment
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mViewPager: ViewPager2
+
+    companion object {
+        private const val PAGE_NUMBERS = 2
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val currentFragment = supportFragmentManager.findFragmentById(R.id.person_list_fragment_container)
-        if (currentFragment == null) {
-            val fragment = PersonListFragment()
-            supportFragmentManager
-                .beginTransaction()
-                .add(R.id.person_list_fragment_container, fragment)
-                .commit()
-        }
+        // set adapter for ViewPager
+        mViewPager = findViewById<ViewPager2>(R.id.viewpager)
+        val viewPagerAdapter = ScreenSlidePagerAdapter(this)
+        mViewPager.adapter = viewPagerAdapter
+    }
+
+    private inner class ScreenSlidePagerAdapter(fa: FragmentActivity): FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = PAGE_NUMBERS
+
+        override fun createFragment(position: Int): Fragment =
+            when (position) {
+                // PersonListFragment
+                0 -> PersonListFragment()
+                else -> Fragment()
+            }
     }
 }
